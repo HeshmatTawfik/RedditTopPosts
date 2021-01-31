a556a1dc74f47d49f3633663513660a081de2f9a
HeshmatTawfik <34392143+HeshmatTawfik@users.noreply.github.com>
2021-02-01 01:29:01 +0200

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/heshmat/reddittopposts/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/heshmat/reddittopposts/ExampleInstrumentedTest.java
@@ -1,6 +1,10 @@
 package com.heshmat.reddittopposts;
 
 import android.content.Context;
+import android.os.Parcel;
+
+import com.heshmat.reddittopposts.models.Children;
+import com.heshmat.reddittopposts.models.Data;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -23,5 +27,20 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.heshmat.reddittopposts", appContext.getPackageName());
+    }
+    @Test
+    public void testParcelable()  {
+        Data data = new Data(false, "thumb", "author", "full name",
+                10, 123456.0, 123456.0, "title", "permalink",
+                "name", "before", "after", "ImgURl");
+        Children myObject = new Children(data,"kind");
+
+        Parcel parcel = Parcel.obtain();
+        myObject.writeToParcel(parcel, 0);
+
+        parcel.setDataPosition(0);
+
+        Children myObjectFromParcel = Children.CREATOR.createFromParcel(parcel);
+        assertEquals(myObject, myObjectFromParcel);
     }
 }
